f142bcccc6cca7724991d6444186a4d9d519cca2
75318070 <yaming.wu@luxsan-ict.com>
2024-02-03 08:08:55 +0800

    桌面应用

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  server: {
+    port: 3004
+  },
   plugins: [
     vue(),
   ],
